d8fc7cdc00d83c7d6144527f57d027e8a98fefda
spfmoby <vrac@netc.eu>
2025-03-26 20:25:14 +0100

    nettoyage toujours

--- view/ajout_transaction.php
+++ view/ajout_transaction.php
@@ -1,20 +1,18 @@
+<?php require_once 'layouts/header.php'; ?>
+
 <?php
 
 // Afficher le formulaire d'ajout de transaction
 ?>
 <h1>Ajout de transaction</h1>
-<form action="controller/CompteController.php?action=ajouterTransactionDepot&id=$_GET['id']" method="post">
+<form action="" method="post">
   <label for="montant">Montant :</label>
+  <input type="number" id="montant" name="montant"><br><br>
+  <label for="type">Type :</label>
   <input type="number" id="montant" name="montant"><br><br>
   <label for="code_PIN">Code PIN :</label>
   <input type="password" id="code_PIN" name="code_PIN"><br><br>
   <input type="submit" value="Ajouter une transaction de dépôt">
 </form>
 
-<form action="controller/CompteController.php?action=ajouterTransactionRetrait&id=$_GET['id']" method="post">
-  <label for="montant">Montant :</label>
-  <input type="number" id="montant" name="montant"><br><br>
-  <label for="code_PIN">Code PIN :</label>
-  <input type="password" id="code_PIN" name="code_PIN"><br><br>
-  <input type="submit" value="Ajouter une transaction de retrait">
-</form>
+<?php require_once 'layouts/footer.php'; ?>
